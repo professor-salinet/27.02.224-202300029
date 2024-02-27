38de698a99009a60cc28571ee1330418f490c906
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-02-27 11:25:44 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,16 @@
+// a linha abaixo é uma declaração da variável "x" que está recendo o valor "1" e encerra com ";"
+var x = 1;
+
+// a linha abaixo é uma declaração da variável "y" que está recendo o valor "1" e encerra com ";"
+var y = 1;
+
+// a linha abaixo é uma declaração do algoritmo IF/ELSE, cuja condição é: se a variável "x" for igual a variável "y", será exibido no log do console a mensagem "Número conferem", caso contrário (else) será exibido no console a mensagem "Números divergem"
+if (x == y){
+    // será exibido no log do console a mensagem "Número conferem"
+    console.log("Números Conferem!")
+// a linha abaixo encerra o bloco IF e inicia o bloco ELSE
+} else {
+    // será exibido no console a mensagem "Números divergem"
+    console.log("Números Divergem!");
+// a linha abaixo encerra o bloco ELSE
+}
